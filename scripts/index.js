c75a3cdeb1b41603833e642a10b199ccbb5998c9
MaxDor <la2web@hotmail.com>
2022-02-02 11:14:49 +0300

    Выполнил урок

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 import start from './modules/start.js';
 import getFormPerson from './modules/formPerson.js';
+import readyPlane from './modules/readyPlane.js';
 
 const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
@@ -10,9 +11,12 @@ const init = (selectorApp, title) => {
         event.preventDefault()
 
         const forms = getFormPerson(firstForm.count.value);
+
         firstForm.remove();
 
         main.append(...forms);
+
+        readyPlane(forms, main);
     });
 };
 
