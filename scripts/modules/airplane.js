bb3c25ee3453fed5d90e87b7b00438d898f2953c
MaxDor <la2web@hotmail.com>
2022-02-03 14:08:48 +0300

    Выполнил урок 3

--- scripts/modules/airplane.js
+++ scripts/modules/airplane.js
@@ -60,7 +60,8 @@ const createBlockSeat = (n, count)=>{
     return fuselage;
 };
 
-const createAirplane = (title, scheme)=>{
+const createAirplane = (title, tourData)=>{
+    const scheme = tourData.scheme;
     const choisesSeat = createElement('form', {
         className: 'choises-seat',
     });
@@ -88,11 +89,41 @@ const createAirplane = (title, scheme)=>{
     return choisesSeat;
 };
 
-const airplane = (main, data)=>{
-    const title = `Выберите  ${declOfNum(data.length, ['место','места','мест'])}`;
-    const scheme = ['exit', 11, 'exit' ,1,'exit',17,'exit'];
+const checkSeat = (form,data)=>{
+    form.addEventListener('change',()=> {
+        const formData = new FormData(form);
+        const checked = [...formData].map(([,value])=> value);
 
-    main.append(createAirplane(title,scheme));
+        if(checked.length === data.length){
+            [...form].forEach(item=>{
+                if (item.checked === false && item.name === 'seat'){
+                    item.disabled = true;
+                }
+            });
+        }
+        
+        });
+
+        form.addEventListener('submint',(event)=>{
+            event.preventDefault();
+            const formData = new FormData(form);
+            const booking = [...formData].map(([,value])=> value);
+
+            for(let i = 0; i < data.length; i++){
+                data[i].seat = booking[i];
+            }
+
+        });
+};
+
+const airplane = (main, data,tourData)=>{
+    const title = `Выберите  ${declOfNum(data.length, ['место','места','мест'])}`;
+
+    const choiseForm = createAirplane(title,tourData);
+
+    checkSeat(choiseForm,data);
+
+    main.append(choiseForm);
 
 };
 
--- scripts/modules/readyPlane.js
+++ scripts/modules/readyPlane.js
@@ -1,6 +1,6 @@
 import airplane from "./airplane.js";
 
-const readyPlane = (forms, main)=> {
+const readyPlane = (forms, main,tourData)=> {
 const data =[];
 
     forms.forEach((form)=>{
@@ -15,7 +15,7 @@ const data =[];
             });
             if(forms.length === data.length){
                 forms.forEach(form=> form.remove());
-                airplane(main, data);
+                airplane(main, data,tourData);
             }
         });
     });
